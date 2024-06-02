08e74fbf0bb0640c551e860a22d8c6208c30fc07
jeenagoyalgoyal <157775366+jeenagoyalgoyal@users.noreply.github.com>
2024-06-02 16:47:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings: ["Database Analyst", "Web Developer", "UI/UX Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
